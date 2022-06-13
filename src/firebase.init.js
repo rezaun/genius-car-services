b7b9e379e1a9091edf22b757a8e254c19fc1d9eb
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-06-13 16:45:41 +0600

    set env

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACQJweOPeWY5phijMwqQiJ5ZtDFYi8L_Q",
-  authDomain: "genius-car-services-ae0a0.firebaseapp.com",
-  projectId: "genius-car-services-ae0a0",
-  storageBucket: "genius-car-services-ae0a0.appspot.com",
-  messagingSenderId: "975771927814",
-  appId: "1:975771927814:web:1328850d42705f7e20ca87"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
