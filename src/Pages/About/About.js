fe10b79313796ed7f6e5b3479c4db29b908bb597
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-07-03 02:36:54 +0600

    page specific title show in the browser has been done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PageTitle from '../Shared/PageTitle/PageTitle';
 
 const ABout = () => {
     return (
         <div>
+            <PageTitle title='About'></PageTitle>
             <h2>This is about page</h2>
         </div>
     );
